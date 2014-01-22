38b31cb28829dab0a4e60d6c8ea934dcf8148340
Wallace Daniel <wdaniel@redhat.com>
2014-01-21 20:18:22 -0500

    moar syntax for sql

--- wsgi/pysosweb.py
+++ wsgi/pysosweb.py
@@ -64,7 +64,7 @@ def new():
 @app.route('/rpm/<name>')
 def rpm_overview(name):
 	# Find each unique rpm in db
-	distinct_rpms = rpmdb.query(rpmdb.name.distinct()).all()
+	distinct_rpms = rpmdb.query(name.distinct()).all()
 	
 	# Make list to store all json objects
 	jsonList = []
