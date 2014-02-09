c012b3848eebc4396ed45bbe25b33ebb4194a133
Wallace Daniel <wdaniel@redhat.com>
2014-02-09 15:01:08 -0500

    added cog glyphicon

--- wsgi/static/js/pysos_app.js
+++ wsgi/static/js/pysos_app.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
 // set mouseover function for each element in the list.html table
 	$(".row-config").parent().mouseover(function() {
 		
-		$(this).children().first().html("<span class="glyphicon glyphicon-cog"></span>");
+	//	$(this).children().first().html("<span class="glyphicon glyphicon-cog"></span>");
 	});
 });
