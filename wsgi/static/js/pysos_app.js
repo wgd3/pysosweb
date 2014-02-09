3b68f5b1028c6cf8abd9200779c988c90c67c34e
Wallace Daniel <wdaniel@redhat.com>
2014-02-09 16:22:57 -0500

    added js to add all the edit rows. i hope.

--- wsgi/static/js/pysos_app.js
+++ wsgi/static/js/pysos_app.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
 
-// set mouseover function for each element in the list.html table
-	$(".row-config").parent().mouseover(function() {
+	// variable for new row
+	var editRow = "<tr class="danger"><td>Test for edit row</td></tr>";
+
+	// insert hidden row after each visible row
+	$("tbody").children().filter("tr").each(function() {
+		$(this).after(editRow)
+	});
+
+	// hide all new rows
+	$(".danger").each(function() {
+		$(this).hide()
+	});
+
+	$(".row-config").click(function() {
+		// should store the row this came from for reference
+		var curRow = $(this).parent();
 		
-	//	$(this).children().first().html("<span class="glyphicon glyphicon-cog"></span>");
+		// slide down editRow
+		curRow.slideDown()
 	});
 });
